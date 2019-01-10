ad59ae1081266d8d98977f45bb124b2ace3376e9
Huy Tom <huyhl@vnext.com.vn>
2019-01-10 10:26:08 +0700

    again

--- cal/calculator.php
+++ cal/calculator.php
@@ -1,18 +1,15 @@
 <html>
 <head>
+<meta charset= "utf-8">
 <title>Simple PHP Calculator</title>
+<link rel="stylesheet" href="style.css"> 
 </head>
 <body>
-<form method='post' action="action.php">
-<input type='text' name='number1'> <br/>
-<!-- <select name='action'> -->
-<!-- <button name= 'action'>+</button>  -->
-<!-- <button name= 'action'>-</button>  -->
-<!-- <button name= 'action'>*</button>  -->
-<!-- <button name= 'action'>/</button> <br/> -->
-<input type='text' name='number2'> <br/>
+<form method='post'>
+<input type='text' name='number1' value ="<?php echo $_POST['number1']?>"> <br/>
+<input type='text' name='number2' value ="<?php echo $_POST['number2']?>"> <br/>
 <select name='action'>
-<option>+</option>
+<option>+</option> 
 <option>-</option>
 <option>*</option>
 <option>/</option> 
@@ -20,5 +17,45 @@
 </select> <br/>
 <input type="submit" name="submit" value='result'> <br/>
 </form>
+<?php
+if(isset($_POST['submit']))
+{
+        $text1 = $_POST['number1'];
+        $text2 = $_POST['number2'];
+        $action = $_POST['action'];
+if(is_numeric($text1) && is_numeric($text2))
+{
+    if($action=="+")
+    {
+        echo "<b> Answer is: </b><br>";
+        echo $text1+$text2;
+    }
+    if($action=="-")
+    {
+        echo "<b> Answer is:</b><br>";
+        echo $text1-$text2;
+    }
+    if($action=="*")
+    {
+        echo "<b> Answer is:</b><br>";
+        echo $text1*$text2;
+    }
+    if($action=="/")
+    {
+        echo "<b> Answer is:</b><br>";
+        echo $text1/$text2;
+    }
+    if($action=="%")
+    {
+        echo "<b> Answer is:</b><br>";
+        echo $text1%$text2;
+    }
+}
+else
+    {
+    echo "Not number";
+    }
+}
+?>
 </body>
 </html>
